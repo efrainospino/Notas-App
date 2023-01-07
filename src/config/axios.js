b3e8c02f3ca21bbb1f1be56edc88476a36eaf23b
Efrain Ospino <efrainospino14@gmail.com>
2023-01-07 10:44:09 -0500

    second production

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:5001'
+    baseURL: 'https://production-na.up.railway.app'
 });
 
 export default clienteAxios;
